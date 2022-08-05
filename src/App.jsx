535dad106b9b7a3393ba88ff563784848c7425cd
Ian Cahn <iancahn@live.com>
2022-08-05 21:09:31 +0200

    First Example done
    Fun facts about React done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import Logo from './img/ReactLogo.png'
+
 function App() {
   return (
     <div className="App">
-      <div className="nav">
-        <img src="./img/ReactLogo.png" alt="Base React Logo" id="logo" />
+      <div className="main">
+        <div className="nav">
+          <img src={Logo} alt="Base React Logo" id="logo" />
+        </div>
+        <div className="content">
+          <h1>Testing the css</h1>
+        </div>
+        <div className="list">
+          <ul>
+            <li>One Item</li>
+            <li>One Item</li>
+            <li>One Item</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
